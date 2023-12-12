399057512e24383549b271c95e2d646488b1d707
jay <valenqbd02@gmail.com>
2023-12-12 18:06:43 +0900

    chore: create folders (components, pages)

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import './App.css'
 
 function App() {
-  return <div className="m-10 text-blue-500">테일윈드확인</div>
+  return (
+    <div className="flex">
+      <div className="m-10 text-slate-300">테일윈드확인</div>
+      <div className="pureCSS">순수css</div>
+    </div>
+  )
 }
 
 export default App
