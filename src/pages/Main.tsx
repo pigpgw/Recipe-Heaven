9b399b957594bdd33192539e8a02b444943983cd
jay <valenqbd02@gmail.com>
2023-12-15 16:15:24 +0900

    chore: pull origin master

---
+++ src/pages/Main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Main() {
+    return (
+      <div>
+        <h5>메인입니다.</h5>
+      </div>
+    )
+}
+
+export default Main;
---
+++ src/pages/Signup.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Signup(){
+    return (
+      <div>
+        <h5>회원가입 페이지입니다.</h5>
+      </div>
+    )
+}
+
+export default Signup;
