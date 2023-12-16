d745c3bbaf90a9b347964530a2f90ead56e2192c
jay <valenqbd02@gmail.com>
2023-12-16 18:45:55 +0900

    chore: reactqueryprovider at app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,28 @@ import RecipeCategoryList from './pages/RecipeCategoryList'
 import Detail from './pages/Detail'
 import Login from './pages/user/Login'
 import Signup from './pages/user/Signup'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: Infinity,
+      },
+    },
+  })
+
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="detail" element={<Detail />} />
-      <Route path="search" element={<RecipeSearchList />} />
-      <Route path="category" element={<RecipeCategoryList />} />
-      <Route path="login" element={<Login />} />
-      <Route path="signUp" element={<Signup />} />
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="detail" element={<Detail />} />
+        <Route path="search" element={<RecipeSearchList />} />
+        <Route path="category" element={<RecipeCategoryList />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signUp" element={<Signup />} />
+      </Routes>
+    </QueryClientProvider>
   )
 }
 
