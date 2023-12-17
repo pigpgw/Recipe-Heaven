862c8126a06847005d888429175f0f94ced83d23
pigpgw <ceh20002@naver.com>
2023-12-17 16:32:48 +0900

    fix: fix after code review

--- src/components/detail/DetailMainReview.tsx
+++ src/components/detail/DetailMainReview.tsx
@@ -7,22 +7,20 @@ type DetailMainReviewProps = {
 const DetailMainReview: React.FC<DetailMainReviewProps> = ({ totalReview }) => {
   
   return (
-    <div className="w-full min-w-[10rem] p-10">
+    <div className="w-full min-w-120px p-10">
       <p
-        className="text-3xl font-bold py-2"
-        style={{ borderBottom: '1px solid black' }}
+        className="text-3xl font-bold py-2 border-b border-black"
       >
-        댓글 <span style={{ color: 'orange' }}>{totalReview.length}</span>
+        댓글 <span className="text-orange-500">{totalReview.length}</span>
       </p>
       {totalReview.map((item, index) => {
         return (
           <div
             key={index}
-            className="w-full py-5 px-1 border-zinc-950"
-            style={{ borderBottom: '1px solid black' }}
+            className="w-full py-5 px-1 border-zinc-950 border-b border-black"
           >
             <div className="flex">
-              <div style={{ color: 'orange' }}>{item.reviewId}</div>
+              <div className="text-orange-500">{item.reviewId}</div>
               <div className="px-3">{item.time}</div>
             </div>
             <div>{item.comment}</div>
