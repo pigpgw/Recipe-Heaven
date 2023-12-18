09ce150b76e1e34fbf799fcc646772190211bd65
jay <valenqbd02@gmail.com>
2023-12-18 16:58:29 +0900

    fix: add isMoreLoading to prevent rapid page increments, feat: tailwind.config theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 export default {
   content: ['./src/**/*.{html,ts,tsx,js,jsx,css}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#B4244A',
+      },
+      fontFamily: {
+        sans: ['Noto Sans KR', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
