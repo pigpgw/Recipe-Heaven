43a9ed1524eee5859dd3dc76318a58973cf82cf0
[SW7] 이다은 <eun000221@gmail.com>
2023-12-19 17:00:02 +0900

    Feat: footer 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Header from '../components/common/Header'
 import MainSearch from "../components/main/MainSearch";
+import Footer from '../components/common/Footer'
 
 
 function Main() {
     return (
       
       <div>
+        <link rel="stylesheet" href="src\components\common\navbar.css"></link>
+        
         <Header/>
         <MainSearch />
-        <h5>메인입니다.</h5>
+        <h5 className="my-36 text-center">메인입니다.</h5>
+        <Footer/>
       </div>
     )
 }
