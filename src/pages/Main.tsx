04160b196277a5443baa4231ce8591090d904f14
[SW7] 이다은 <eun000221@gmail.com>
2023-12-21 18:24:48 +0900

    Feat: 메인화면 라우팅, UI, 상품API

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,19 +2,25 @@ import React from "react";
 import Header from '../components/common/Header'
 import MainSearch from "../components/main/MainSearch";
 import Footer from '../components/common/Footer'
-
+import RankItem from "../components/main/RankItem";
 
 function Main() {
-    return (
-      
-      <div>
-        {/* <link rel="stylesheet" href="src\components\common\navbar.css"></link> */}
-        <Header/>
-        <MainSearch />
-        <h5 className="my-36 text-center">메인입니다.</h5>
-        <Footer/>       
+  return (
+
+    <div>
+      <Header />
+      <MainSearch />
+      {/* 현재 이미지 -> 우선순위 낮음: caroucel*/}
+      <div className="flex items-center justify-center mt-10 mb-16">
+        <img src="\src\assets\main\caroucel_1.jpg" alt="" className="w-[64rem]" />
       </div>
-    )
+      <span className="flex flex-col items-center text-2xl font-bold my-7">이달의 인기 레시피 {'>'}</span>
+      <RankItem />
+      <span className="flex flex-col items-center text-2xl font-bold my-7">달콤한 인기 레시피 {'>'}</span>
+      <RankItem />
+      <Footer />
+    </div>
+  )
 }
 
 export default Main;
