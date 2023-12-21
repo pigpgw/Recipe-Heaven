4333bd05b67c4165e1ae2f25b102081942f65667
jay <valenqbd02@gmail.com>
2023-12-21 19:34:23 +0900

    chore: delete log

--- src/fetch/fetchTestGet.ts
+++ src/fetch/fetchTestGet.ts
@@ -3,8 +3,7 @@ import { ListBySearchAPIResponse } from './APIResponsesTypes'
 import { QueryFunction } from '@tanstack/react-query'
 
 const fetchTestGet: QueryFunction<string> = async ({ queryKey }) => {
-  const { keyword, items, page } = queryKey[1]
-  console.log(page, '페이지받아 패치실행')
+  const { keyword, category, items, page } = queryKey[1]
 
   try {
     const apiRes = await axios.get(
