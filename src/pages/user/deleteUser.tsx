cc0921adf50b173b13a4ff7b800cf5dd4d197056
[SW7] 최재혁 <wogur9817@gmail.com>
2023-12-21 15:07:00 +0900

    feat:nicknameEdit.tsx

--- src/pages/user/deleteUser.tsx
+++ src/pages/user/deleteUser.tsx
@@ -3,31 +3,25 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
 
-const DeleteUser: React.FC = () => {
+const DeleteUser = () => {
   const [isProcessing, setProcessing] = useState(false);
   const userNavigate = useNavigate();
 
   const handleDeleteUser = async () => {
-    // 사용자 확인 창
     const confirmMessage = '정말로 탈퇴하시겠습니까?';
     const isConfirmed = window.confirm(confirmMessage);
 
     if (!isConfirmed) {
-      // 사용자가 취소한 경우
       return;
     }
 
     try {
       setProcessing(true);
-      // 회원 삭제를 위한 API 호출
       await axios.delete('https://jsonplaceholder.typicode.com/posts/1');
-      // 성공적으로 회원 삭제한 경우 다시 로그인 페이지로 이동 또는 다른 처리
       userNavigate('/login');
-      // 성공 메시지
       toast.success('회원 탈퇴가 성공적으로 처리되었습니다.');
     } catch (error) {
       console.error('회원 삭제 중 에러:', error);
-      // 에러 메시지
       toast.error('회원 탈퇴 중 오류가 발생했습니다.');
     } finally {
       setProcessing(false);
@@ -38,9 +32,9 @@ const DeleteUser: React.FC = () => {
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-2xl font-bold mb-4">회원 탈퇴</h1>
       <p className="mb-6">탈퇴하면 앞으로 이 계정으로 로그인할 수 없고 이 계정을 다시 복구할 수 없습니다<br></br>
-        * 닉네임을 변경하고 싶다면 마이페이지안의 닉네임 변경 탭에서 변경할 수 있습니다.<br></br>
+        * 닉네임을 변경하고 싶다면 마이페이지 안의 닉네임 변경 탭에서 변경할 수 있습니다.<br></br>
         * 공공의 성격을 가진 게시물은 탈퇴 후에도 삭제되지 않으므로 반드시 직접 삭제하신 후 탈퇴해 주시기 바랍니다.</p>
-        <p className="mb-8 text-xl font-bold">탈퇴하려면 아래 확인버튼을 클릭해주세요.</p>
+        <p className="mb-8 text-xl font-bold">탈퇴하려면 아래 확인 버튼을 클릭해주세요.</p>
       <div className="mb-8">
         <button
           className="bg-primary text-white px-4 py-2 rounded disabled:bg-gray-400 disabled:cursor-not-allowed"
--- src/pages/user/nicknameEdit.tsx
+++ src/pages/user/nicknameEdit.tsx
@@ -1,91 +1,80 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
 import axios from 'axios';
 import { toast } from 'react-hot-toast';
 
-const NicknameEdit: React.FC = () => {
+const NicknameEdit = () => {
   const [id, setId] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    const fetchId = async () => {
+    const fetchName = async () => {
       try {
-        // 서버에서 id 데이터 가져오기 (id = 닉네임)
         const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
+        console.log('응답 데이터:', response.data);
         if (response.data && response.data.id) {
-          // 성공적으로 데이터를 가져왔을 때
           setId(response.data.id.toString());
           setLoading(false);
         } else {
-          // 서버 응답에 유효한 데이터가 없을 때
           console.error('서버 응답에 유효한 데이터가 없습니다.');
           setLoading(false);
         }
       } catch (error) {
-        // 데이터 가져오기 중 오류가 발생했을 때
         console.error('닉네임 불러오기 중 오류 발생:', error);
         setLoading(false);
       }
     };
 
-    // 데이터 가져오기 함수 호출
-    fetchId();
-  }, []); // 빈 배열 전달
+    fetchName();
+  }, []);
 
-  const handleIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    // 닉네임 입력 값 변경 시 호출
+  const handleIdChange = (e: ChangeEvent<HTMLInputElement>) => {
     setId(e.target.value);
   };
 
-  const handleIdSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleIdSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
   
     try {
       if (validateId()) {
-        // 닉네임 변경 요청
-        const response = await axios.put(`https://jsonplaceholder.typicode.com/posts/${id}`, {});
+        const response = await axios.patch('https://jsonplaceholder.typicode.com/posts/1', {});
+  
+        console.log('응답 데이터:', response.data); // 응답 데이터를 콘솔에 출력
   
         if (response.data && response.data.id) {
-          // 서버 응답이 성공하면 변경된 닉네임으로 변경 
           toast.success('닉네임이 변경되었습니다');
           setId(response.data.id.toString());
         }
       }
     } catch (error) {
-      // 닉네임 변경 중 오류가 발생했을 때
       console.error('닉네임 변경 중 오류 발생:', error);
       toast.error('닉네임 변경 중 오류가 발생했습니다.');
     }
   };
 
   const validateId = (): boolean => {
-    // 닉네임 유효성 검사
     if (id.length < 2 || id.length > 8) {
       setError('닉네임은 최소 2글자에서 최대 8글자 사이어야 합니다.');
       toast.error('닉네임은 최소 2글자에서 최대 8글자 사이어야 합니다.');
       return false;
     }
 
-    // 유효성 검사 통과 시 에러 메시지
     setError(null);
     return true;
   };
 
   if (loading) {
-    // 데이터를 가져오는 중일 때 로딩 메시지
     return <div>Loading...</div>;
   }
-  // 데이터를 모두 가져온 후에 닉네임 변경 페이지 렌더링
+
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="container mx-auto p-4">
         <h1 className="text-2xl font-bold mb-4">닉네임 변경</h1>
         <form onSubmit={handleIdSubmit} className="max-w-md">
-          {/* 현재 닉네임 표시 */}
           <label className="block mb-2">
             현재 닉네임: {id || '없음'}
           </label>
-          {/* 새로운 닉네임 입력 */}
           <label className="block mb-4">
             새로운 닉네임:
             <input
@@ -96,9 +85,7 @@ const NicknameEdit: React.FC = () => {
               className="border p-2 w-full"
             />
           </label>
-          {/* 에러 메시지가 있으면 표시 */}
           {error && <p className="text-red-500 mb-2">{error}</p>}
-          {/* 변경 버튼 */}
           <button
             type="submit"
             className="bg-primary text-white py-2 px-4 rounded"
