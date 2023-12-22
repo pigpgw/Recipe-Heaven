56e7b6d461bf2d6ab30270b062e11d5be6fc626d
jay <valenqbd02@gmail.com>
2023-12-23 00:54:40 +0900

    chore: check font

--- src/pages/myPage/myComments.tsx
+++ src/pages/myPage/myComments.tsx
@@ -67,6 +67,7 @@ function MyComments() {
   return (
     <div>
       <h2 className="font-sans">댓글 목록({commentList.length})</h2>
+      <h2>댓글 목록({commentList.length})</h2>
       <div className="flex flex-col justify-center w-full">
         <div>
           <input
