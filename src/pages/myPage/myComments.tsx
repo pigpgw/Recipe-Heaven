862a9259af8c963f476f2f27ef097f395111a26e
jay <valenqbd02@gmail.com>
2023-12-23 00:34:24 +0900

    chore: change css

--- src/pages/myPage/myComments.tsx
+++ src/pages/myPage/myComments.tsx
@@ -66,7 +66,7 @@ function MyComments() {
 
   return (
     <div>
-      <h2>찜한레시피({commentList.length})</h2>
+      <h2 className="font-sans">댓글 목록({commentList.length})</h2>
       <div className="flex flex-col justify-center w-full">
         <div>
           <input
