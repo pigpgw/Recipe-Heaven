3ed3429375f136f2ad202f89cd1607206b0b7e48
jay <valenqbd02@gmail.com>
2023-12-22 11:55:34 +0900

    chore: change fileName in myPage

---
+++ src/components/myPage/MypageCommentItem.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { TempRecipe } from '../../fetch/APIResponsesTypes'
+import { useDeleteLikeMutation } from '../likes/useLikesMutation'
+import moment from 'moment'
+
+const MypageCommentItem = ({ comment }: TempRecipe) => {
+  const { deleteRecipe, isDeleting } = useDeleteLikeMutation(comment.id)
+  const commentDate = new Date()
+  const momentDate = moment(commentDate)
+  const formattedDate = momentDate.format('YYYY-MM-DD, h:mm')
+
+  return (
+    <div className="flex gap-3">
+      <img
+        className="w-20 h-20 object-cover"
+        src={
+          'https://images.unsplash.com/photo-1604908176997-125f25cc6f3d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fCVFQyU5QSU5NCVFQiVBNiVBQ3xlbnwwfHwwfHx8MA%3D%3D'
+        }
+        alt={`${comment.title}의 메인이미지`}
+      />
+
+      <div className="flex flex-col">
+        <div>{comment.comment}</div>
+        <div>{comment.title}</div>
+        <div>{formattedDate}</div>
+      </div>
+      <button
+        onClick={() => {
+          deleteRecipe(comment.id)
+        }}
+        disabled={isDeleting}
+      >
+        X
+      </button>
+    </div>
+  )
+}
+
+export default MypageCommentItem
