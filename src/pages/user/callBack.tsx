69dba9e5d2d03bc47aa37959bf3705b99361ed92
[SW7] 최재혁 <wogur9817@gmail.com>
2023-12-23 16:41:07 +0900

    feat: callback.tsx

--- src/pages/user/callBack.tsx
+++ src/pages/user/callBack.tsx
@@ -1,45 +1,46 @@
 import React, { useEffect } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
 import axios from 'axios';
-import useTokenStore from '../../components/store/tokenStore';
-
+import { useNavigate } from 'react-router-dom';
 
 const Callback = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const { accessToken, setAccessToken } = useTokenStore();
-
   useEffect(() => {
-    // 서버에 인가코드 전송하는 함수
-    const sendAuthorizationCodeToServer = async (code: string) => {
-      try {
-        const response = await axios.post('backendToken', {
-          code,
-        });
+    const userNavigate = useNavigate();
+    const handleAuthorizationCode = async () => {
+      // URL에서 인가코드 추출
+      const urlParams = new URLSearchParams(window.location.search);
+      const authorizationCode = urlParams.get('code');
 
-        // 토큰 스토어에서 받은 토큰 설정
-        setAccessToken(response.data.accessToken);
+      if (authorizationCode) {
+        try {
+          // 백엔드 서버의 URL
+          const backendUrl = 'https://backendServerUrl/auth/kakao';
 
-        navigate('/nickname');
-      } catch (error) {
-        console.error('서버로 인가 코드를 전송하는 과정에서 오류가 발생:', error);
+          // 인가코드를 백엔드로 전송
+          const response = await axios.post(backendUrl, {
+            code: authorizationCode,
+          });
+
+          // 서버에서 받은 응답 처리
+          console.log('백엔드에서 받은 응답:', response.data);
+
+          // 상태 업데이트
+
+          userNavigate('/nickname');
+        } catch (error) {
+          console.error('서버로 인가 코드를 전송하는 과정에서 오류가 발생:', error);
+        }
+      } else {
+        console.error('인가 코드를 찾을 수 없음');
       }
     };
-    // 현재 URL에서 인가코드 파라미터 값 가져오기
-    const authorizationCode = new URLSearchParams(location.search).get('code');
-    // 서버에 인가 코드 전송 함수 호출
-    if (authorizationCode) {
-      sendAuthorizationCodeToServer(authorizationCode);
-      console.log('인가 코드:', authorizationCode);
-    } else {
-      console.error('인가 코드를 찾을 수 없음');
-    }
-  }, [location, navigate, setAccessToken]);
+
+    // 인가코드 처리 함수 호출
+    handleAuthorizationCode();
+  }, []);
 
   return (
     <div>
       콜백 페이지
-      <div></div>
     </div>
   );
 };
