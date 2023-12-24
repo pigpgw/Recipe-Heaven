772d12ec65caa817b658d50ca4b7cb6af4b54eca
jay <valenqbd02@gmail.com>
2023-12-24 22:13:28 +0900

    feat: 댓글 컴포넌트

--- src/components/review/Reiview.tsx
+++ src/components/review/Reiview.tsx
@@ -1,12 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import StarRating from './StarRating'
+import toast from 'react-hot-toast'
 
 const Review = () => {
+  const [selectedRating, setSelectedRating] = useState(0)
+  const [commentContent, setCommentContent] = useState('')
+
+  const handleRatingChange = (rating: number): void => {
+    setSelectedRating(rating)
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault()
+    if (selectedRating === 0) {
+      toast.error('별점을 입력해주세요')
+      return
+    }
+    if (commentContent.trim() === '') {
+      toast.error('내용을 입력해주세요')
+      return
+    }
+
+    const formData = new FormData()
+    formData.append('commentContent', commentContent)
+
+    setSelectedRating(0)
+    setCommentContent('')
+  }
   return (
-    <div>
-      <h2>댓글</h2>
-      <StarRating />
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="starRating">별점:</label>
+        <StarRating
+          selectedRating={selectedRating}
+          onRatingChange={handleRatingChange}
+        />
+      </div>
+      <div>
+        <textarea
+          id="commentContent"
+          name="commentContent"
+          value={commentContent}
+          onChange={(e) => setCommentContent(e.target.value)}
+        />
+      </div>
+      <button type="submit">작성완료</button>
+    </form>
   )
 }
 export default Review
