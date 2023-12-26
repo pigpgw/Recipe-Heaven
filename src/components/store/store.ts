eeb1b44d185b464577e3b7b938e0e0601c642532
jay <valenqbd02@gmail.com>
2023-12-26 16:33:57 +0900

    chore: store  store 변경사항 업데이트

--- src/components/store/store.ts
+++ src/components/store/store.ts
@@ -1,13 +1,16 @@
+import { access } from 'fs/promises'
 import { create } from 'zustand'
 import { devtools, persist } from 'zustand/middleware'
 
-export interface LikedState {
+export interface StoreState {
   likedRecipes: string[]
   toggleLikedRecipe: (recipeId: string) => void
   isLiked: (recipeId: string) => boolean
+  accessToken: string | null
+  setAccessToken: (token: string | null) => void
 }
 
-export const useStore = create<LikedState>()(
+export const useStore = create<StoreState>()(
   devtools(
     persist(
       (set, get) => ({
@@ -20,6 +23,15 @@ export const useStore = create<LikedState>()(
               ? state.likedRecipes.filter((id) => id !== recipeId)
               : [...state.likedRecipes, recipeId],
           })),
+        accessToken: null,
+        setAccessToken: (newToken) => {
+          if (newToken === null || typeof newToken !== 'string') {
+            console.error('유효하지 않은 토큰 형식입니다.')
+          } else {
+            set(() => ({ accessToken: newToken }))
+          }
+        },
+        getAccessToken: () => get().accessToken,
       }),
       { name: 'store' },
     ),
