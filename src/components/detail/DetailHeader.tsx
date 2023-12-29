28d287275fb35de4a9142cc96dc1b6de8e591f06
[SW7] 이다은 <eun000221@gmail.com>
2023-12-30 07:54:57 +0900

    Chore: dev 최신 반영

--- src/components/detail/DetailHeader.tsx
+++ src/components/detail/DetailHeader.tsx
@@ -8,54 +8,58 @@ function DetailHeader({ fetchData }) {
     return null
   }
 
+  console.log('fetchData', fetchData.img)
+
   return (
-    <div className="flex flex-wrap items-center justify-center min-w-[50rem]  w-5/6">
-      <div className="w-2/5 p-6 min-w-[25rem]">
+    <div className="flex flex-wrap items-center justify-center min-w-[50rem]  w-5/6 ml-[5rem]">
+      <div className="w-2/5 px-6 min-w-[25rem]">
         {/* 서버에서 이미지 받아오기 */}
         <img
-          className="w-full h-[30rem]"
+          className="min-w-[27rem] h-[30rem]"
           // 수정 필요
           // src="../src/assets/detail/cake.jpeg"
-          src={`../src/assets/common/크리스마스/당근라페샌드위치/main.jpeg`}
+          // src={`../src/assets/common/크리스마스/당근라페샌드위치/main.jpeg`}
+          src={fetchData.img}
+          // src="https://lh3.google.com/u/0/d/1wIHHKzAMpOqm6vHVebqBu_oAYq0-oKL8=w2880-h1392-iv1"
           alt=""
         />
       </div>
-      <div className="w-3/5 p-8">
-        <div className="w-full">
+      <div className="w-3/5">
+        <div className="w-full m-4">
           <div className="flex justify-between">
-            <div className="text-2xl font-bold">
-              요리명 : {fetchData.recipeName}
+            <div className="text-2xl font-bold pl-10">
+              {fetchData.recipeName}
             </div>
             <div className="flex justify-center items-center p-2">
-              <img
-                className="w-[2rem]"
-                src="./src/assets/detail/다운로드.png"
+              {/* <img
+                className="w-[3rem] h-[3rem]"
+                src="../src/assets/detail/다운로드.png"
                 alt=""
-              />
+              /> */}
               <h4 className="p-1">{fetchData.Like}</h4>
             </div>
           </div>
 
           <div className="flex items-center justify-around p-1">
             <div className="flex items-center flex-col">
-              <div className="p-7 text-4xl text-slate-500">
-                <IoIosPeople style={{ height: 30 }} />
+              <div className="p-4 text-4xl text-slate-500">
+                <IoIosPeople style={{ height: 40 }} />
               </div>
               <p className="text-xs text-slate-500 font-bold">
                 {fetchData.portion}
               </p>
             </div>
             <div className="flex items-center flex-col">
-              <div className="p-7 text-3xl text-slate-500">
-                <FaRegClock style={{ height: 30 }} />
+              <div className="p-4 text-3xl text-slate-500">
+                <FaRegClock style={{ height: 40 }} />
               </div>
               <p className="text-xs text-slate-500 font-bold">
                 {fetchData.leadTime}
               </p>
             </div>
             <div className="flex items-center flex-col">
-              <div className="p-7 text-3xl text-slate-500">
-                <TbStarsFilled style={{ height: 30 }} />
+              <div className="p-4 text-3xl text-slate-500">
+                <TbStarsFilled style={{ height: 40 }} />
               </div>
               <p className="text-xs text-slate-500 font-bold">
                 {fetchData.level}
@@ -64,21 +68,19 @@ function DetailHeader({ fetchData }) {
           </div>
         </div>
 
-        <div className="py-2">
-          <p className="text-1xl py-4 font-extrabold">
+        <div className="py-2 flex items-center justify-center flex-col">
+          <p className="text-1xl pl-20 py-2 font-extrabold w-full">
             재료 및 분량{' '}
             <span className="text-orange-600">{fetchData.portion}</span>
           </p>
-          <div className="h-[14rem] bg-gray-100 p-7 font-medium">
-            <div className="flex w-full">
-              {fetchData?.ingredient?.map((item, index) => {
-                return (
-                  <div key={index} className="p-2">
-                    {item.item} {item.unit}
-                  </div>
-                )
-              })}
-            </div>
+          <div className="bg-gray-100 px-8 pt-3 font-medium flex flex-wrap w-9/12 min-h-[17rem]">
+            {fetchData?.ingredient?.map((item, index) => {
+              return (
+                <div key={index} className="p-2 w-1/2">
+                  {`${item.item} ${item.unit}`}
+                </div>
+              )
+            })}
           </div>
         </div>
       </div>
--- src/components/detail/DetailMainList.tsx
+++ src/components/detail/DetailMainList.tsx
@@ -1,23 +1,17 @@
 import React from 'react'
 
-// type DetailMainProps = {
-//   explaincontentList: string[]
-//   sequenceImgList: string[]
-// }
-
 function DetailMain({ explaincontentList, sequenceImgList }) {
   console.log('sequenceImgList', sequenceImgList)
   return (
     <div className="flex flex-col bg-white w-5/6">
       {explaincontentList.map((item, index) => {
-        console.log(sequenceImgList[index])
-        const imageIndex = index + 1
-
+        // console.log(sequenceImgList[index])
+        // const imageIndex = index + 1
         return (
           <div key={index}>
             <FoodMakingList
-              // sequenseImgUrl={sequenceImgList[index]}
-              sequenseImgUrl={imageIndex}
+              sequenseImgUrl={sequenceImgList[index]}
+              // sequenseImgUrl={imageIndex}
               explainText={item}
               index={index}
             />
@@ -28,48 +22,44 @@ function DetailMain({ explaincontentList, sequenceImgList }) {
   )
 }
 
-// type FoodMakingListProps = {
-//   sequenseImgUrl: string
-//   explainText: string
-//   index: number
-// }
-
-        // const imageFileName = `sandwich${imageIndex}.jpeg`
-        // const imagePath = `assets/common/크리스마스/당근라페샌드위치/sandwich/${imageFileName}`
-
 function FoodMakingList({
   sequenseImgUrl,
   explainText,
   index,
 }) {
-  console.log('sequenseImgUrlsequenseImgUrl',sequenseImgUrl)
   return (
     <>
       {sequenseImgUrl ? (
-        <div className="flex  justify-center p-10 flex-wrap w-full">
-          <div className="w-9/12 px-2 py-2 flex">
-            <div className="text-2xl w-7 h-10 bg-orange-600 flex items-center justify-center rounded-3xl">
-              {index + 1}
+        <div className="flex  justify-center p-10 flex-wrap w-full items-center ">
+          <div className="w-8/12 py-2 flex">
+            <div className="text-2xl  flex items-center justify-center rounded-3xl">
+              <div className="w-10 h-10 bg-primary text-white rounded-3xl flex items-center justify-center">
+                {index + 1}
+              </div>
             </div>
-            <p className="px-5 m-2  min-w-[15rem]">{explainText}</p>
+            <p className="px-3 m-2  min-w-[10rem]">{explainText}</p>
           </div>
           <div className="w-3/12 min-w-[10rem]">
             <img
               className="w-[25rem] h-[15rem] rounded-2xl"
               // src={sequenseImgUrl}
               // src="../src/assets/common/logo.png"
-              src={`../src/assets/common/크리스마스/당근라페샌드위치/sandwich${sequenseImgUrl}.jpeg`}
+              // src={`../src/assets/common/크리스마스/당근라페샌드위치/sandwich${sequenseImgUrl}.jpeg`}
+              src={sequenseImgUrl}
               alt=""
             />
           </div>
         </div>
       ) : (
         <div className="flex  justify-center p-10 flex-wrap">
-          <div className="w-9/12 px-10 py-5 flex">
-            <div className="text-2xl w-10 h-10 bg-orange-600 flex items-center justify-center rounded-3xl">
+          <div className="w-8/12 py-2 flex mb-[10rem]">
+            <div className="text-2xl w-10 h-10 bg-primary text-white flex items-center justify-center rounded-3xl">
               {index + 1}
             </div>
-            <p className="px-5 m-2  min-w-[15rem]">{explainText}</p>
+            <p className="px-3 m-2  min-w-[10rem]">{explainText}</p>
+          </div>
+          <div className="w-3/12 min-w-[10rem]">
+            <div className="w-[22rem] h-[15rem] rounded-2xl"></div>
           </div>
         </div>
       )}
